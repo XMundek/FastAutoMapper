248c07644164a909cf06ae3a1262a7b8fea1385e
Mirosław Gałat <xmundek@gmail.com>
2019-01-08 01:41:47 +0100

    Upgrade project files to VS 2017

--- FastAutoMapperUnitTests/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-[assembly: AssemblyTitle("Moon.FastAutoMapper.UnitTest")]
-[assembly: AssemblyCompany("MoonSoft Company Ltd.")]
-[assembly: AssemblyProduct("Moon.FastAutoMapper.UnitTest")]
-[assembly: AssemblyCopyright("Copyright © MoonSoft Company Ltd. 2017")]
-[assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.1.2.0")]
-[assembly: AssemblyFileVersion("1.1.2.0")]
--- Moon.FastAutoMapper.PerformanceTest/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-[assembly: AssemblyTitle("Moon.FastAutoMapper.PerformanceTest")]
-[assembly: AssemblyCompany("MoonSoft Company Ltd.")]
-[assembly: AssemblyProduct("Moon.FastAutoMapper.PerformanceTest")]
-[assembly: AssemblyCopyright("Copyright © MoonSoft Company Ltd. 2017")]
-[assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.1.2.0")]
-[assembly: AssemblyFileVersion("1.1.2.0")]
